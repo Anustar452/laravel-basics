8532baedf52f564597ff023e7543966c810863f9
Anu-al-habesha <59417257+Anu-al-habesha@users.noreply.github.com>
2023-02-16 10:03:37 +0300

    view created

--- todoList/routes/web.php
+++ todoList/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');  
 });
+Route::post('/saveItemRoute', function () {
+    return view('welcome');  
+})->name('saveItem');
